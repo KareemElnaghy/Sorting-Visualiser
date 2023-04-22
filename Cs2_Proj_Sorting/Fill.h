93eddf43afd645b3b859bc4b18845bce3b3b39f7
mostafa21314 <mostafa21314@aucegypt.edu>
2023-04-22 12:37:31 +0200

    pushing

--- Cs2_Proj_Sorting/Fill.h
+++ Cs2_Proj_Sorting/Fill.h
@@ -4,18 +4,18 @@
 #include <ctime>
 inline void Fill(int arr[], int num)
 {
- //srand(time(NULL));
- //for (int i = 0; i < num; i++)//loop that fills the array with the required data elements( numbers from 1-num)
- //{
-	// arr[i] = i;
- //}
- //for (int i = 2; i<num; i++)//loop that shuffles the content of the array randomly
- //{
-	// int j = (rand() % i)+1;
-	// int temp = arr[i];
-	// arr[i] = arr[j];
-	// arr[j] = temp;
- //}
+ srand(time(NULL));
+ for (int i = 0; i < num; i++)//loop that fills the array with the required data elements( numbers from 1-num)
+ {
+	 arr[i] = i;
+ }
+ for (int i = 2; i<num; i++)//loop that shuffles the content of the array randomly
+ {
+	 int j = (rand() % i)+1;
+	 int temp = arr[i];
+	 arr[i] = arr[j];
+	 arr[j] = temp;
+ }
 
 }
 #endif
